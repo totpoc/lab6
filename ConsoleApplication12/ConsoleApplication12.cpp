66b6ac70256777c6d6ab7591518925a6a1475ab7
spais <maksim.ryabcew@gmail.com>
2020-12-25 01:11:31 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int spacePosition = 0;
+    bool consistsOfNumbers = false, charactersEnded = false;
+    string inputString;
+    cout << "Введите строку: ";
+    getline(cin, inputString);
+    cout << "Итоговая строка:";
+    for (int i = 0; i < inputString.length(); i++)
+    {
+        if (inputString[i] == ' ' || i == inputString.length() - 1)
+        {
+            cout << " ";
+            consistsOfNumbers = false;
+            for (int j = spacePosition; j < i; j++)
+            {
+                if (inputString[j] == ' ')
+                    j++;
+                if (inputString[j] <= 57 && inputString[j] >= 48)
+                    consistsOfNumbers = true;
+                else
+                {
+                    consistsOfNumbers = false;
+                    break;
+                }
+            }
+            for (int j = spacePosition; j <= i; j++)
+            {
+                charactersEnded = false;
+                if (inputString[j] == ' ' && j == spacePosition)
+                {
+                    j++;
+                }
+                
+                if (!consistsOfNumbers) {
+                    if (((inputString[j] >= 122 || inputString[j] <= 65) || ((inputString[j] <= 96 && inputString[j] >= 90) && inputString[j] != 95)) && !charactersEnded)
+                    {
+                        inputString[j] = ' ';
+                    }
+                    else {
+                        charactersEnded = true;
+                        cout << inputString[j];
+                    }
+                }
+            }
+            spacePosition = i;
+            
+        }
+    }
+   
+
+}
+
